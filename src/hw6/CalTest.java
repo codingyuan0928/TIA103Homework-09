114e9acbf5c9b733e546c14f8b0accacff37cddc
Leon Chang <godknows963852741@gmail.com>
2024-07-31 11:29:07 +0800

    hw6 upload

---
+++ src/hw6/CalTest.java
@@ -0,0 +1,23 @@
+package hw6;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CalTest {
+	public static void main(String[] args) {
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("請輸入x的值:");
+			int x = sc.nextInt();
+			System.out.println("請輸入y的值:");
+			int y = sc.nextInt();
+			Calculator c = new Calculator();
+			c.powerXY(x, y);
+			sc.close();
+		}catch(CalException e) {
+			System.out.println(e.getMessage());
+		}catch(InputMismatchException e) {
+			System.out.println("輸入格式不正確");
+		}
+	}
+}
