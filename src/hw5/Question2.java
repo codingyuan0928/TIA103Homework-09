075140c0944b3a02cfce9857b073190fb1daaf3c
Leon Chang <godknows963852741@gmail.com>
2024-08-06 13:56:00 +0800

    hw3&hw5 problem fixed

--- src/hw5/Question2.java
+++ src/hw5/Question2.java
@@ -5,7 +5,7 @@ public class Question2 {
 		int[] x = new int[10];
 
 		for (int i = 0; i < 10; i++) {
-			x[i] = (int) Math.floor(Math.random() * 101);
+			x[i] = (int) (Math.random() * 101);
 		}
 		Question2 q = new Question2();
 		q.randAvg(x);
